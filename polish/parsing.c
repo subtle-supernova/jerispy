f4546ebacd73fe1a4a1a4549f2366453ab4a5a68
Jeremy Sullivan <suddenlysullivan@gmail.com>
2016-07-01 16:41:39 +0000

    q expressions

--- polish/parsing.c
+++ polish/parsing.c
@@ -21,7 +21,7 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
-enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
+enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 typedef struct lval {
@@ -64,11 +64,20 @@ lval* lval_sexpr(void) {
   return v;
 }
 
+lval* lval_qexpr(void) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_QEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
+
 void lval_del(lval* v) {
   switch(v->type) {
     case LVAL_NUM: break;
     case LVAL_ERR: free(v->err); break;
     case LVAL_SYM: free(v->sym); break;
+    case LVAL_QEXPR:
     case LVAL_SEXPR:
                    for (int i = 0; i < v->count; i++) {
                      lval_del(v->cell[i]);
@@ -100,6 +109,7 @@ lval* lval_read(mpc_ast_t* t) {
   lval* x = NULL;
   if(strcmp(t-> tag, ">") == 0) { x = lval_sexpr(); }
   if(strcmp(t->tag, "sexpr")) { x = lval_sexpr(); }
+  if(strstr(t->tag, "qexpr")) { x = lval_qexpr(); }
 
   for ( int i = 0; i < t->children_num; i++ ) {
     if(strcmp(t->children[i]->contents, "(") == 0) { continue; }
@@ -134,6 +144,7 @@ void lval_print(lval* v) {
     case LVAL_ERR: printf("Error: %s", v->err); break;
     case LVAL_SYM: printf("%s", v->sym); break;
     case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
+    case LVAL_QEXPR: lval_expr_print(v, '{', '}'); break;
   }
 }
 
@@ -267,6 +278,7 @@ int main(int argc, char** argv) {
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Symbol = mpc_new("symbol");
   mpc_parser_t* Sexpr = mpc_new("sexpr");
+  mpc_parser_t* Qexpr = mpc_new("qexpr");
   mpc_parser_t* Expr = mpc_new("expr");
   mpc_parser_t* Jerispy = mpc_new("jerispy");
 
@@ -274,10 +286,11 @@ int main(int argc, char** argv) {
         "number : /-?[0-9]+/ ; \
         symbol : '+' | '-' | '*' | '/' | '%' ; \
         sexpr: '(' <expr>* ')' ; \
-        expr : <number> | <symbol> | <sexpr> ; \
+        qexpr: '{' <expr>* '}' ; \
+        expr : <number> | <symbol> | <sexpr> | <qexpr> ; \
         jerispy : /^/ <expr>* /$/ ; \
       ",
-      Number, Symbol, Sexpr, Expr, Jerispy);
+      Number, Symbol, Sexpr, Qexpr, Expr, Jerispy);
   puts("Jerispy Version 0.0.3");
   puts("Press Ctrl+c to exit\n");
 
@@ -305,6 +318,6 @@ int main(int argc, char** argv) {
   }
 
   //MPC cleanup
-  mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Jerispy);
+  mpc_cleanup(4, Number, Symbol, Sexpr, Qexpr, Expr, Jerispy);
   return 0;
 }
